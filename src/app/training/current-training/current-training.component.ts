39f29d74d7c63ccc5dde10ac22a1646d98cb0731
ChristianKatka <ckatka@hotmail.com>
2020-02-26 12:30:03 +0200

    pop up dialog done

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 // Need to import pop up dialog
 import { MatDialog } from '@angular/material/dialog';
 // Pop up dialog to show
@@ -13,6 +13,9 @@ import { StopTrainingComponent } from './stop-training.component';
 })
 export class CurrentTrainingComponent implements OnInit {
 
+  @Output() trainingExit = new EventEmitter<any>();
+
+
   // renders progress on graphical spinner
   progress = 0;
   timer;
@@ -20,24 +23,48 @@ export class CurrentTrainingComponent implements OnInit {
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
-    
+    this.startOrResumeTimer();
+  }
+
+  startOrResumeTimer() {
+
     /**Timer starts to run when user comes to this component
      * 
      * if progress gets to 100% timer will be stopped
-     * shows false error
      */
-    this.timer = setInterval(()=> {
+    this.timer = setInterval(() => {
       this.progress = this.progress + 5;
-      if(this.progress >= 100) {
+      if (this.progress >= 100) {
         clearInterval(this.timer);
       }
     }, 1000)
-  
+
   }
 
+  /** When user presses stop training button pop up dialog opens "Are you sure" 
+   * 
+   * StopTrainingComponent: injection MAT_DIALOG_DATA receives second parameter so data can be send to pop up dialog
+   * 
+   */
   onStop() {
     clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent);
+    // return referense to current dialog
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // If user pressed YES button to exit training
+      if (result) {
+        // new-training.component.html Listens this emit call
+        this.trainingExit.emit();
+      }
+      else {
+        this.startOrResumeTimer();
+      }
+    });
   }
 
 
--- src/app/training/current-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 
 @Component({
     selector: 'app-stop-training',
     template: `
     <h1 mat-dialog-title>Are you sure?</h1>
+    <mat-dialog-content>
+        <p>You already got {{ passedData.progress }}%</p>
+    </mat-dialog-content>
     <mat-dialog-actions>
     <button mat-button [mat-dialog-close]="true">Yes</button>
     <button mat-button [mat-dialog-close]="false">No</button>
@@ -12,5 +16,14 @@ import { Component } from "@angular/core";
     `
 })
 
-export class StopTrainingComponent { }
+export class StopTrainingComponent { 
+    
+    /** Get progress data to this pop up component
+     * 
+     * @param passedData object that has progress value.
+     * "you have already accomplished 10% of the work out are you sure you want to quit"
+     */
+    constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {}
+}
+
 
